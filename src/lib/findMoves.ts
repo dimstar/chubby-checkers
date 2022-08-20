1b933102ec201219f643bb5facd887f2ec76b8d2
dimstar <kylermagee@gmail.com>
2022-08-20 14:45:09 -0700

    immuatbly sets moves

--- src/lib/findMoves.ts
+++ src/lib/findMoves.ts
@@ -1,6 +1,7 @@
-import { isEqual } from "lodash";
-import { GameTileState, Players } from "../types";
-import deepCopy from "../utils/deepCpoy";
+import { isEqual } from 'lodash';
+import { GAME_ORIGIN } from '../constants';
+import { GameTileState, Players } from '../types';
+import deepCopy from '../utils/deepCpoy';
 
 interface Direction {
   x: number;
@@ -28,66 +29,64 @@ const possibleAttacks: Directions = {
   sw: { x: -2, y: 2 }
 };
 
-  /**
-   * Returns a closure to handle board position selected by current player
-   */
-   function findMoves(
-    currentPlayer: Players,
-    opponent: Players,
-    activePiece: number[],
-    gameState: GameTileState[][]
-  ): GameTileState[][] {
+/**
+ * Returns a closure to handle board position selected by current player
+ */
+function findMoves(
+  currentPlayer: Players,
+  opponent: Players,
+  activePiece: number[],
+  gameState: GameTileState[][]
+): GameTileState[][] {
+  const newGameState = deepCopy(GAME_ORIGIN);
 
-    const newGameState = deepCopy(gameState);
-
-    gameState.forEach((x, xi) =>
-      x.forEach((y, yi) => {
-        const attemptCoords = [xi, yi];
-        // check for any valid move
-        (Object.keys(possibleMoves) as (keyof typeof possibleMoves)[]).forEach(
-          (dr) => {
-            const checkMove = possibleMoves[dr];
-            const checkAttack = possibleAttacks[dr];
-            const ax = activePiece[0];
-            const ay = activePiece[1];
-            // null check
-            if (ax === null || ay === null) {
-              return;
-            }
-            // ignore out of bounds
-            if (newGameState[possibleMoves[dr].x + ax] === undefined) {
-              return;
-            }
-            if (
-              newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] ===
-              undefined
-            ) {
-              return;
-            }
-            // if move is valid, set player
-            if (isEqual(attemptCoords, [checkMove.x + ax, checkMove.y + ay])) {
-              newGameState[ax][ay] = null;
-              newGameState[attemptCoords[0]][attemptCoords[1]] = currentPlayer;
-            }
-            // if attack is valid, do nothing
-            if (
-              isEqual(attemptCoords, [checkAttack.x + ax, checkAttack.y + ay]) &&
-              newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] ===
-                opponent
-            ) {
-              // action was valid, change game state and clear the active piece
-              newGameState[ax][ay] = null;
-              newGameState[attemptCoords[0]][attemptCoords[1]] = currentPlayer;
-              newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] =
-                null;
-            }
+  gameState.forEach((x, xi) =>
+    x.forEach((y, yi) => {
+      const attemptCoords = [xi, yi];
+      // check for any valid move
+      (Object.keys(possibleMoves) as (keyof typeof possibleMoves)[]).forEach(
+        (dr) => {
+          const checkMove = possibleMoves[dr];
+          const checkAttack = possibleAttacks[dr];
+          const ax = activePiece[0];
+          const ay = activePiece[1];
+          // null check
+          if (ax === null || ay === null) {
+            return;
           }
-        );
-      })
-    )
+          // ignore out of bounds
+          if (newGameState[possibleMoves[dr].x + ax] === undefined) {
+            return;
+          }
+          if (
+            newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] ===
+            undefined
+          ) {
+            return;
+          }
+          // if move is valid, set player
+          if (isEqual(attemptCoords, [checkMove.x + ax, checkMove.y + ay])) {
+            newGameState[ax][ay] = null;
+            newGameState[attemptCoords[0]][attemptCoords[1]] = currentPlayer;
+          }
+          // if attack is valid, do nothing
+          if (
+            isEqual(attemptCoords, [checkAttack.x + ax, checkAttack.y + ay]) &&
+            newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] ===
+              opponent
+          ) {
+            // action was valid, change game state and clear the active piece
+            newGameState[ax][ay] = null;
+            newGameState[attemptCoords[0]][attemptCoords[1]] = currentPlayer;
+            newGameState[possibleMoves[dr].x + ax][possibleMoves[dr].y + ay] =
+              null;
+          }
+        }
+      );
+    })
+  );
 
-
-    return newGameState;
-  }
+  return newGameState;
+}
 
 export default findMoves;
